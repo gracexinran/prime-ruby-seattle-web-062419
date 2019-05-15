253e033f3a0156fa1581096c3a08a20ce3795842
Xinran Che <gracexinran@gmail.com>
2019-05-15 22:32:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,24 @@
-# Add  code here!
-def prime?(number)
-  if number < 2
+# # Add  code here!
+# def prime?(number)
+#   if number < 2
+#     return false
+#   elsif number == 2
+#     return true
+#   elsif number > 2
+#     (2...number).each do |i|
+#       if number % i == 0
+#         return false
+#       end
+#     end
+#     return true
+#   end
+# end
+def prime?(num)
+  if num < 0 or num == 0 or num == 1
     return false
-  elsif number == 2
-    return true
-  elsif number > 2
-    (2...number).each do |i|
-      if number % i == 0
-        return false
-      end
+  else
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
     end
-    return true
   end
 end
